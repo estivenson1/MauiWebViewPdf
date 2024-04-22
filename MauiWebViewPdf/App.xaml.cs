d7cca184873f044668b3d0ba5e1236dd8e61fe5b
Estivenson Ortega Villa <estivenson.ortega@softwareone.com>
2024-04-22 18:36:14 -0500

    Creación del proyecto Maui MauiWebViewPdf

---
+++ MauiWebViewPdf/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiWebViewPdf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiWebViewPdf/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiWebViewPdf
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
